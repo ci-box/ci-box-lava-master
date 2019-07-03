6435f4d83c169d8cb66d149f996e07966d4a70fb
Loic Poulain <loic.poulain@linaro.org>
2019-07-03 15:58:05 +0200

    populate_device: change wildcard
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>

--- scripts/populate_devices.sh
+++ scripts/populate_devices.sh
@@ -13,10 +13,12 @@ for worker in ${WORKERS}; do
 	lava-server manage workers add ${worker}
 done
 
+# define definition should be named devicetype_idx@worker.jinja2
+
 for file in ${DEV_DIR}/*.jinja2; do
-	type=`basename $file | cut -d- -f1`
+	type=`basename $file | cut -d_ -f1`
 	device=`basename $file | cut -d. -f1`
-	worker=`basename $file | rev | cut -d- -f2 | rev`
+	worker=`basename $file | cut -d@ -f2 | cut -d. -f1`
 	echo "ADDING device <${device}>, type <${type}> to worker <${worker}>"
 	lava-server manage devices add --device-type ${type} --worker ${worker} ${device}
 done
