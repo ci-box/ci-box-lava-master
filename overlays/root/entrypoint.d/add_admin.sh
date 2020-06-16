167f540f8a11565c63f6729741ae72e4cb1ab3bc
Loic Poulain <loic.poulain@linaro.org>
2020-06-16 16:18:22 +0200

    Fix add admin script
    Never fail
    
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>

--- overlays/root/entrypoint.d/add_admin.sh
+++ overlays/root/entrypoint.d/add_admin.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-set -x
+set +x
 
 lava-server manage users add ${ADMIN_USERNAME} --passwd ${ADMIN_PASSWORD} --email ${ADMIN_EMAIL} --superuser --staff 2>/dev/null
 if [ ! -z ${ADMIN_TOKEN} ]; then
         lava-server manage tokens add --user ${ADMIN_USERNAME} --description "Admin token" --secret ${ADMIN_TOKEN}
 fi
 
+exit 0
